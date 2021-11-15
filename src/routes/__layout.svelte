5c024c9f3cd38779b362c7b9fc76ab53f96cada4
Faraaz Baig <fazzy0908@yandex.com>
2021-11-16 01:12:50 +0530

    seo optimaizations

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,6 +13,40 @@
 
 <svelte:head>
 	<title>Airbound | Delivery made simple</title>
+	<meta name="title" content="Airbound | Delivery made Simple" />
+	<meta
+		name="description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+	<meta
+		name="keywords"
+		content="drone, drone delivery, airbound, airbound aerospace, logistics, last mile delivery, VTOL, fixed wing, local businesses, medicines, "
+	/>
+	<meta name="robots" content="index, follow" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="English" />
+	<meta name="revisit-after" content="4 days" />
+	<meta name="author" content="Airbound " />
+	<meta property="og:title" content="Airbound " />
+	<meta property="og:site_name" content="Airbound" />
+	<meta property="og:url" content="https://airbound.co" />
+	<meta
+		property="og:description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://airbound.co/banner_img.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Airbound" />
+	<meta
+		name="twitter:description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+	<meta name="twitter:url" content="https://airbound.co" />
+	<meta name="twitter:image" content="https://airbound.co/banner_image.png" />
+	<meta name="twitter:image:alt" content="https://airbound.co/banner_image.png" />
+	<meta name="twitter:creator" content="@airbound_co" />
+	<meta name="twitter:site" content="@airbound_co" />
 </svelte:head>
 
 <div class={`relative overflow-x-hidden ${$open ? 'overflow-hidden' : ''}  `}>
