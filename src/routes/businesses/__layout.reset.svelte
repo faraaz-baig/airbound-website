b4ead5b5d5b4648f23905cabfbd92359a4fcdb7e
Faraaz Baig <fazzy0908@yandex.com>
2021-11-26 20:30:28 +0530

    svelte-seo lib added to fix seo

--- src/routes/businesses/__layout.reset.svelte
+++ src/routes/businesses/__layout.reset.svelte
@@ -5,49 +5,46 @@
 	import Footer from '../../components/footer.svelte';
 	import { open } from '../../stores/sidebar';
 	import { onMount } from 'svelte';
+	import SvelteSeo from 'svelte-seo';
 
 	onMount(() => {
 		window.scrollTo(0, 0);
 	});
 </script>
 
-<svelte:head>
-	<title>Airbound | Delivery made simple</title>
-	<meta name="title" content="Airbound | Delivery made Simple" />
-	<meta
-		name="description"
-		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
-	/>
-	<meta
-		name="keywords"
-		content="drone, drone delivery, airbound, airbound aerospace, logistics, last mile delivery, VTOL, fixed wing, local businesses, medicines, "
-	/>
-	<meta name="robots" content="index, follow" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="English" />
-	<meta name="revisit-after" content="4 days" />
-	<meta name="author" content="Airbound " />
-	<meta property="og:title" content="Airbound " />
-	<meta property="og:site_name" content="Airbound" />
-	<meta property="og:url" content="https://airbound.co" />
-	<meta
-		property="og:description"
-		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
-	/>
-	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://airbound.co/banner_image.png" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="Airbound" />
-	<meta
-		name="twitter:description"
-		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
-	/>
-	<meta name="twitter:url" content="https://airbound.co" />
-	<meta name="twitter:image" content="https://airbound.co/banner_image.png" />
-	<meta name="twitter:image:alt" content="https://airbound.co/banner_image.png" />
-	<meta name="twitter:creator" content="@airbound_co" />
-	<meta name="twitter:site" content="@airbound_co" />
-</svelte:head>
+<SvelteSeo
+	title="Airbound | Delivery made Simple"
+	description="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+/>
+
+<SvelteSeo
+	openGraph={{
+		title: 'Airbound | Delivery made Simple',
+		description:
+			'Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone.',
+		url: 'https://airbound.co/',
+		type: 'website',
+		images: [
+			{
+				url: 'https://airbound.co/banner_image.png',
+				width: 1200,
+				height: 630,
+				alt: 'Og Image Alt'
+			}
+		]
+	}}
+/>
+
+<SvelteSeo
+	twitter={{
+		site: '@airbound_co',
+		title: 'Airbound | Delivery made Simple',
+		description:
+			'Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone.',
+		image: 'https://airbound.co/banner_image.png',
+		imageAlt: 'Airbound Delivery Simplified Image'
+	}}
+/>
 
 <div class={`relative overflow-x-hidden ${$open ? 'overflow-hidden' : ''}  `}>
 	<BizNavbar />
