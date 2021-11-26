4331bce3f2f55a08eabcf91b4dd79e834c2c4ac1
Faraaz Baig <fazzy0908@yandex.com>
2021-11-26 21:55:17 +0530

    layout seo added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,38 @@
 	});
 </script>
 
+<svelte:head>
+	<!-- HTML Meta Tags -->
+	<title>Airbound | Delivery made Simple</title>
+	<meta
+		name="description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://airbound.co/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Airbound | Delivery made Simple" />
+	<meta
+		property="og:description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+	<meta property="og:image" content="https://airbound.co/banner_image.png" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="airbound.co" />
+	<meta property="twitter:url" content="https://airbound.co/" />
+	<meta name="twitter:title" content="Airbound | Delivery made Simple" />
+	<meta
+		name="twitter:description"
+		content="Airbound is a convenient service that allows you to shop in your city and support local business and get your order delivered via our drone."
+	/>
+	<meta name="twitter:image" content="https://airbound.co/banner_image.png" />
+
+	<!-- Meta Tags Generated via https://www.opengraph.xyz -->
+</svelte:head>
+
 <div class={`relative overflow-x-hidden ${$open ? 'overflow-hidden' : ''}  `}>
 	<Navbar />
 	<Sidebar />
